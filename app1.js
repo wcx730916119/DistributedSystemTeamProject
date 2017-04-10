c67704a5220791def2b7c8e861b0e1b171820822
Rajkiran <rajkiran2190@gmail.com>
2017-04-10 08:57:03 -0700

    need to check react

---
+++ app1.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+ddadjaslhcals
+class App extends Component {
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1>Welcome to Peer2Pair Programming Platform</h1>
+                </div>
+                <video id="local_pc" autoPlay muted/>
+                <video id="remote_pc" autoPlay/>
+            </div>
+
+        );
+    }
+}
+
+export default App;
