7219caab282d4e4173d2f7eea13f4c1db0ec932f
Rajkiran <rajkiran2190@gmail.com>
2017-04-28 22:02:45 -0700

    test post messages

--- paxos/paxos/paxos_impl.go
+++ paxos/paxos/paxos_impl.go
@@ -9,10 +9,11 @@ import (
 	"net/rpc"
 	"sync"
 	"time"
-    "encoding/json"
-    "strconv"
-    "bytes"
+	"encoding/json"
+	"strconv"
+
 	"net/url"
+	"strings"
 )
 
 /* Node specific information */
@@ -34,8 +35,8 @@ type paxosNode struct {
 	CommitStore map[string]interface{}
 
 	ConnectNodesChannel chan bool
-    
-    /* Mutex to synchronize access to above maps */
+
+	/* Mutex to synchronize access to above maps */
 	Lock                sync.Mutex
 
 	client string
@@ -57,7 +58,7 @@ type proposeResult struct {
 
 /* Marshalled and returned in ReplaceCatchupReply */
 type catchupData struct {
-    /* Maps keys to Highest proposal # seen for that key */
+	/* Maps keys to Highest proposal # seen for that key */
 	ProposalState map[string]state
 
 	/* Maps committed keys to values */
@@ -77,16 +78,16 @@ func (pn *paxosNode) debugPrint(a ...interface{}) {
 // numRetries: number of retries to connect to each node
 // replace: flag for if it is a replacement node for a failed node
 func NewPaxosNode(myHostPort string, hostMap map[int]string, numNodes, srvId, numRetries int, replace bool, client string) (PaxosNode, error) {
-	
+
 	node := paxosNode{HostMap: make(map[string]string), ServerID: srvId, ConnectionMap: make(map[string]*rpc.Client),
 		ConnectNodesChannel: make(chan bool), ProposalState: make(map[string]state),
 		NumNodes: numNodes, CommitStore: make(map[string]interface{})}
-    
-    for id, hostport := range hostMap {
+
+	for id, hostport := range hostMap {
 		debugPrint(srvId, myHostPort, id, hostport)
-        node.HostMap[strconv.Itoa(srvId)] = hostport
+		node.HostMap[strconv.Itoa(srvId)] = hostport
 	}
-    /* Register RPC */
+	/* Register RPC */
 	rpc.RegisterName("PaxosNode", paxosrpc.Wrap(&node))
 	rpc.HandleHTTP()
 	l, e := net.Listen("tcp", myHostPort)
@@ -94,71 +95,71 @@ func NewPaxosNode(myHostPort string, hostMap map[int]string, numNodes, srvId, nu
 		return nil, e
 	}
 	go http.Serve(l, nil)
-    
+
 	node.client = client
 
-    if replace {
-        node.debugPrint("replace", myHostPort)
-        replaceServerChannel := make(chan bool)
-        var restorePoint *catchupData
-        
-        /* Send replaceServer rpc to all other nodes */
-        for id, hostport := range hostMap {
-            client, _ := rpc.DialHTTP("tcp", hostport)
-            node.ConnectionMap[strconv.Itoa(id)] = client
-            if id != srvId {
-                
-                go func(hostport string) {
-                    var rep paxosrpc.ReplaceServerReply
-                    rpcArgs := &paxosrpc.ReplaceServerArgs{SrvID: srvId, Hostport: myHostPort}
-                    client.Call("PaxosNode.RecvReplaceServer", rpcArgs, &rep)
-                    replaceServerChannel <- true
-                    
-                }(hostport)
-            }
-        }
-        
-        /* Wait for all replace server RPC calls to complete. */
-        for i := 0; i < numNodes - 1; i++ {
-            <-replaceServerChannel
-        }
-        
-        /* Makes sure the node that we send replacecatchup is different from this node */
-        catchupID := 0
-        if srvId == catchupID {
-            catchupID ++
-        }
-        var rep paxosrpc.ReplaceCatchupReply
-        rpcArgs := &paxosrpc.ReplaceCatchupArgs{}
-        client := node.ConnectionMap[strconv.Itoa(catchupID)]
-        client.Call("PaxosNode.RecvReplaceCatchup", rpcArgs, &rep)
-        json.Unmarshal(rep.Data, &restorePoint)
-        node.debugPrint(rep.Data, restorePoint)
+	if replace {
+		node.debugPrint("replace", myHostPort)
+		replaceServerChannel := make(chan bool)
+		var restorePoint *catchupData
 
-        for K, V := range restorePoint.ProposalState {
-            node.ProposalState[K] = V
-        }
-        for K, V := range restorePoint.CommitStore {
-            node.CommitStore[K] = V
-        }
-    
-    } else {
-        /* Attempt a connection from this node to all nodes */
-        for id, hostport := range hostMap {
-            //debugPrint(srvId, id, hostport)
-            go node.Connect(id, hostport, numRetries)
-        }
-        
-        /* Wait for completed node connections */
-        completedNodes := 0
-        for completedNodes < numNodes {
-            completion := <-node.ConnectNodesChannel
-            if !completion {
-                return nil, errors.New("Connection failed with a node")
-            }
-            completedNodes++
-        }
-    }
+		/* Send replaceServer rpc to all other nodes */
+		for id, hostport := range hostMap {
+			client, _ := rpc.DialHTTP("tcp", hostport)
+			node.ConnectionMap[strconv.Itoa(id)] = client
+			if id != srvId {
+
+				go func(hostport string) {
+					var rep paxosrpc.ReplaceServerReply
+					rpcArgs := &paxosrpc.ReplaceServerArgs{SrvID: srvId, Hostport: myHostPort}
+					client.Call("PaxosNode.RecvReplaceServer", rpcArgs, &rep)
+					replaceServerChannel <- true
+
+				}(hostport)
+			}
+		}
+
+		/* Wait for all replace server RPC calls to complete. */
+		for i := 0; i < numNodes - 1; i++ {
+			<-replaceServerChannel
+		}
+
+		/* Makes sure the node that we send replacecatchup is different from this node */
+		catchupID := 0
+		if srvId == catchupID {
+			catchupID ++
+		}
+		var rep paxosrpc.ReplaceCatchupReply
+		rpcArgs := &paxosrpc.ReplaceCatchupArgs{}
+		client := node.ConnectionMap[strconv.Itoa(catchupID)]
+		client.Call("PaxosNode.RecvReplaceCatchup", rpcArgs, &rep)
+		json.Unmarshal(rep.Data, &restorePoint)
+		node.debugPrint(rep.Data, restorePoint)
+
+		for K, V := range restorePoint.ProposalState {
+			node.ProposalState[K] = V
+		}
+		for K, V := range restorePoint.CommitStore {
+			node.CommitStore[K] = V
+		}
+
+	} else {
+		/* Attempt a connection from this node to all nodes */
+		for id, hostport := range hostMap {
+			//debugPrint(srvId, id, hostport)
+			go node.Connect(id, hostport, numRetries)
+		}
+
+		/* Wait for completed node connections */
+		completedNodes := 0
+		for completedNodes < numNodes {
+			completion := <-node.ConnectNodesChannel
+			if !completion {
+				return nil, errors.New("Connection failed with a node")
+			}
+			completedNodes++
+		}
+	}
 	fmt.Println("Created node ", srvId)
 	return &node, nil
 }
@@ -169,8 +170,8 @@ func NewPaxosNode(myHostPort string, hostMap map[int]string, numNodes, srvId, nu
 func (pn *paxosNode) Connect(srvID int, hostport string, numRetries int) {
 
 	client, e := rpc.DialHTTP("tcp", hostport)
-    
-    /* Sleeps for a second in between each connection attempt */
+
+	/* Sleeps for a second in between each connection attempt */
 	for numRetries > 0 && e != nil {
 		numRetries--
 		time.Sleep(time.Second)
@@ -181,7 +182,7 @@ func (pn *paxosNode) Connect(srvID int, hostport string, numRetries int) {
 		pn.ConnectionMap[strconv.Itoa(srvID)] = client
 		pn.Lock.Unlock()
 		debugPrint(pn.ServerID, "successfully connected to", srvID, hostport)
-        
+
 		pn.ConnectNodesChannel <- true
 	} else {
 		pn.ConnectNodesChannel <- false
@@ -193,9 +194,9 @@ func (pn *paxosNode) GetNextProposalNumber(args *paxosrpc.ProposalNumberArgs, re
 	defer pn.Lock.Unlock()
 
 	data, ok := pn.ProposalState[args.Key]
-    
-    /* Each N generated by this method for node with srvID x will satisfy
-     * N % pn.NumNodes == x */
+
+	/* Each N generated by this method for node with srvID x will satisfy
+	 * N % pn.NumNodes == x */
 	if ok {
 		data.My_n = (data.N_h/pn.NumNodes+1)*pn.NumNodes + pn.ServerID
 	} else {
@@ -210,21 +211,21 @@ func (pn *paxosNode) GetNextProposalNumber(args *paxosrpc.ProposalNumberArgs, re
 func (pn *paxosNode) Propose(args *paxosrpc.ProposeArgs, reply *paxosrpc.ProposeReply) error {
 	timeout := time.After(15 * time.Second)
 	returnChan := make(chan proposeResult)
-    pn.debugPrint("Proposing")
+	pn.debugPrint("Proposing")
 	go pn.DoPropose(args, returnChan)
 	for {
 		select {
 		case result := <-returnChan:
 
 			if !result.Success {
-                /* Not sure what to do if we fail to get majority. Just wait
-                 * and try again for now */
+				/* Not sure what to do if we fail to get majority. Just wait
+				 * and try again for now */
 				time.Sleep(3 * time.Second)
 				go pn.DoPropose(args, returnChan)
 			} else {
 				commitChannel := make(chan bool)
-                
-                /* Send commit RPC call to all nodes. */
+
+				/* Send commit RPC call to all nodes. */
 				for srv, client := range pn.ConnectionMap {
 					pn.debugPrint("Prepare commit", srv, client)
 					go func(args *paxosrpc.ProposeArgs, client *rpc.Client, commitChannel chan bool) {
@@ -234,8 +235,8 @@ func (pn *paxosNode) Propose(args *paxosrpc.ProposeArgs, reply *paxosrpc.Propose
 						commitChannel <- true
 					}(args, client, commitChannel)
 				}
-                
-                /* Wait for all commit RPC calls to complete. */
+
+				/* Wait for all commit RPC calls to complete. */
 				for i := 0; i < pn.NumNodes; i++ {
 					<-commitChannel
 				}
@@ -265,14 +266,14 @@ func (pn *paxosNode) DoPropose(args *paxosrpc.ProposeArgs, channel chan proposeR
 		go func(prepareChan chan paxosrpc.PrepareReply, args *paxosrpc.ProposeArgs, client *rpc.Client) {
 			var reply paxosrpc.PrepareReply
 			rpcArgs := &paxosrpc.PrepareArgs{Key: args.Key, N: args.N}
-            pn.debugPrint("Send prepare", args.Key, args.N)
+			pn.debugPrint("Send prepare", args.Key, args.N)
 			client.Call("PaxosNode.RecvPrepare", rpcArgs, &reply)
 			prepareChan <- reply
 		}(prepareChan, args, client)
 	}
-    
-    /* Wait for RPC calls to complete until we reach a majority and know that
-     * we have failed to obtain a majoirty */
+
+	/* Wait for RPC calls to complete until we reach a majority and know that
+	 * we have failed to obtain a majoirty */
 	for numOK < majority && numFail < pn.NumNodes-majority+1 {
 		select {
 		case reply := <-prepareChan:
@@ -304,7 +305,7 @@ func (pn *paxosNode) DoPropose(args *paxosrpc.ProposeArgs, channel chan proposeR
 
 		go func(acceptChan chan paxosrpc.AcceptReply, args *paxosrpc.ProposeArgs, client *rpc.Client) {
 			var reply paxosrpc.AcceptReply
-            pn.debugPrint("Send Accept", args.Key, args.N)
+			pn.debugPrint("Send Accept", args.Key, args.N)
 			rpcArgs := &paxosrpc.AcceptArgs{Key: args.Key, N: args.N, V: V_a}
 			client.Call("PaxosNode.RecvAccept", rpcArgs, &reply)
 			acceptChan <- reply
@@ -347,7 +348,7 @@ func (pn *paxosNode) GetValue(args *paxosrpc.GetValueArgs, reply *paxosrpc.GetVa
 func (pn *paxosNode) RecvPrepare(args *paxosrpc.PrepareArgs, reply *paxosrpc.PrepareReply) error {
 	pn.Lock.Lock()
 	defer pn.Lock.Unlock()
-	
+
 	data, ok := pn.ProposalState[args.Key]
 	if !ok {
 		data = state{N_a: 0, N_h: 0, My_n: 0}
@@ -355,12 +356,12 @@ func (pn *paxosNode) RecvPrepare(args *paxosrpc.PrepareArgs, reply *paxosrpc.Pre
 	}
 	if args.N < data.N_h {
 		*reply = paxosrpc.PrepareReply{Status: paxosrpc.Reject}
-        pn.debugPrint("Received Prepare", args.Key, args.N, "REJECT", data.N_h)
+		pn.debugPrint("Received Prepare", args.Key, args.N, "REJECT", data.N_h)
 	} else {
 		data.N_h = args.N
 		*reply = paxosrpc.PrepareReply{Status: paxosrpc.OK, N_a: data.N_a, V_a: data.V_a}
 		pn.ProposalState[args.Key] = data
-        pn.debugPrint("Received Prepare", args.Key, args.N, "ACCEPT")
+		pn.debugPrint("Received Prepare", args.Key, args.N, "ACCEPT")
 	}
 	return nil
 
@@ -377,12 +378,12 @@ func (pn *paxosNode) RecvAccept(args *paxosrpc.AcceptArgs, reply *paxosrpc.Accep
 	}
 	if args.N < data.N_h {
 		*reply = paxosrpc.AcceptReply{Status: paxosrpc.Reject}
-        pn.debugPrint("Received Prepare", args.Key, args.N, "REJECT", data.N_h)
+		pn.debugPrint("Received Prepare", args.Key, args.N, "REJECT", data.N_h)
 	} else {
 		data = state{N_a: args.N, N_h: args.N, My_n: data.My_n, V_a: args.V}
 		*reply = paxosrpc.AcceptReply{Status: paxosrpc.OK}
 		pn.ProposalState[args.Key] = data
-        pn.debugPrint("Received Prepare", args.Key, args.N, "ACCEPT")
+		pn.debugPrint("Received Prepare", args.Key, args.N, "ACCEPT")
 	}
 	return nil
 }
@@ -397,7 +398,7 @@ func (pn *paxosNode) RecvCommit(args *paxosrpc.CommitArgs, reply *paxosrpc.Commi
 		pn.debugPrint("Commit failed")
 		pn.ProposalState[args.Key] = state{N_a: 0, N_h: 0, My_n: 0}
 	} else {
-        	//Clean up N_a and V_a in case we get another proposal for the same key
+		//Clean up N_a and V_a in case we get another proposal for the same key
 		pn.debugPrint("Commit succeeded")
 		data.N_a = 0
 		data.V_a = nil
@@ -416,43 +417,48 @@ func (pn *paxosNode) RecvCommit(args *paxosrpc.CommitArgs, reply *paxosrpc.Commi
 
 func (pn *paxosNode) postDiffToClient( args *paxosrpc.CommitArgs ){
 	fmt.Println("I am trying to post something")
-	apiUrl := pn.client
-	resource := "/update"
+	apiUrl := "http://" + pn.client + "/update"
+	fmt.Println(apiUrl)
 	data := url.Values{}
 	data.Set("key", args.Key)
 	data.Add("diff", args.V.(string))
-	u, _ := url.ParseRequestURI(apiUrl)
-	u.Path = resource
-	urlStr := fmt.Sprintf("%v", u) // "https://api.com/user/"
-	client := &http.Client{}
-	r, _ := http.NewRequest("POST", urlStr, bytes.NewBufferString(data.Encode())) // <-- URL-encoded payload
-	r.Header.Add("Authorization", "auth_token=\"XXXXXXX\"")
-	r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	r.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
-	resp, _ := client.Do(r)
+	s := data.Encode()
+	fmt.Printf("v.Encode(): %v\n", s)
+	req, err := http.NewRequest("POST", apiUrl, strings.NewReader(s))
+	if err != nil {
+		fmt.Printf("http.NewRequest() error: %v\n", err)
+		return
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	c := &http.Client{}
+	resp, err := c.Do(req)
+	if err != nil {
+		fmt.Printf("http.Do() error: %v\n", err)
+		return
+	}
 	fmt.Println(resp.Status)
 }
 
 func (pn *paxosNode) RecvReplaceServer(args *paxosrpc.ReplaceServerArgs, reply *paxosrpc.ReplaceServerReply) error {
 	pn.debugPrint("Replace Server", args.SrvID)
-    client, e := rpc.DialHTTP("tcp", args.Hostport)
-    if e != nil {
-        return e
-    }
-    pn.Lock.Lock()
-    pn.ConnectionMap[strconv.Itoa(args.SrvID)] = client
-    pn.HostMap[strconv.Itoa(args.SrvID)] = args.Hostport
-    pn.Lock.Unlock()
+	client, e := rpc.DialHTTP("tcp", args.Hostport)
+	if e != nil {
+		return e
+	}
+	pn.Lock.Lock()
+	pn.ConnectionMap[strconv.Itoa(args.SrvID)] = client
+	pn.HostMap[strconv.Itoa(args.SrvID)] = args.Hostport
+	pn.Lock.Unlock()
 	return nil
 }
 
 func (pn *paxosNode) RecvReplaceCatchup(args *paxosrpc.ReplaceCatchupArgs, reply *paxosrpc.ReplaceCatchupReply) error {
-    pn.debugPrint("Replace catchup")
-    pn.debugPrint(pn.HostMap, pn.ConnectionMap, pn.ProposalState, pn.CommitStore)
+	pn.debugPrint("Replace catchup")
+	pn.debugPrint(pn.HostMap, pn.ConnectionMap, pn.ProposalState, pn.CommitStore)
 
-    cd := catchupData{CommitStore: pn.CommitStore, ProposalState: pn.ProposalState}
-    bytes, e := json.Marshal(cd)
-    pn.debugPrint(bytes, e)
-    *reply = paxosrpc.ReplaceCatchupReply{Data: bytes }
+	cd := catchupData{CommitStore: pn.CommitStore, ProposalState: pn.ProposalState}
+	bytes, e := json.Marshal(cd)
+	pn.debugPrint(bytes, e)
+	*reply = paxosrpc.ReplaceCatchupReply{Data: bytes }
 	return nil
 }
